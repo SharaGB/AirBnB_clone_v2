85aab8dada5c15cb5cdebfcade6dba8525d5e4ed
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-15 14:42:33 -0500

    Added Place attributes to store in db. Foreign key issues

--- models/city.py
+++ models/city.py
@@ -3,12 +3,18 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
+    # Name of the Table
     __tablename__ = 'cities'
 
+    # Attributes/columns
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    
+    # Relationships to other tables
+    places = relationship("Place", cascade="all, delete", backref="cities")
