599dfcb4743def972ad282113632d33393982117
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-15 20:58:16 -0500

    Add conditional for models class

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 import models
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, null
 
 Base = declarative_base()
 
@@ -28,6 +28,10 @@ class BaseModel:
                     value = datetime.strptime(value, date)
                 if key != '__class__':
                     setattr(self, key, value)
+            if not "id" in self.__dict__:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
