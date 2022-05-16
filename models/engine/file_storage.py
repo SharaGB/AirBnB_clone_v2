03b6ebecc4c5a843fe309d9e16da1cab2d2fe49b
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-16 16:38:01 -0500

    Fix task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.user import User
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.amenity import Amenity
-from models.base_model import BaseModel
-
-classes = {
-    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-    'State': State, 'City': City, 'Amenity': Amenity,
-    'Review': Review
-}
 
 
 class FileStorage:
