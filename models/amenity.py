3735552abcad5e6fe2525b8339a26ce74e445b0a
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-16 16:25:40 -0500

    Update class Amenity adding environment variable

--- models/amenity.py
+++ models/amenity.py
@@ -2,12 +2,15 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
+# from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """ Inherits from BaseModel and Base """
     __tablename__ = 'amenities'
 
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity', backref='amenities')
+    name = Column(String(128), nullable=False
+    ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    # place_amenities = relationship('Place', secondary='place_amenity',
+    #                                backref='amenities')
