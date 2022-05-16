3747a760f8149a9c918e54c58d56cbdb118ddbc6
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-16 10:43:22 -0500

    Add test cases for the HBNBCommand class

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,54 +1,35 @@
-import os
 import pep8
-import json
 import unittest
-from models.engine.file_storage import FileStorage
-from datetime import datetime
+import console
 from unittest import TestCase
-from models.state import State
-from console import HBNBCommand
 
 
-class Test_pep8(TestCase):
-    '''Test pep8'''
-    def test_pep8(self):
-        """Test that we conforms to PEP8.
-        """
-        style = pep8.StyleGuide()
-        result = style.check_files(["console.py"])
-        self.assertEqual(result.total_errors, 0, "Fix pep8")
+class TestConsole(TestCase):
+    """ Unittests """
 
+    def test_pep8_conformance_tests(self):
+        """ Test that we conform to PEP8. """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-class Test_docstring(TestCase):
-    '''Test doc strings '''
-    def test_docstring(self):
-        """ Test doc strings """
-        self.assertIsNotNone(State.__doc__)
+    def test_pep8_conformance_console(self):
+        """ Test that we conform to PEP8. """
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-
-class Test_console(TestCase):
-    """ test class City """
-    def setUp(self):
-        """Clean code after each test
-        """
-        if os.path.isfile("file.json"):
-            os.remove("file.json")
-        FileStorage.FileStorage__objects = {}
-        if os.path.isfile("salida"):
-            os.remove("salida")
-
-    def test_create(self):
-        ''' test the method create'''
-        # Chack if create the file.json
-        os.system('echo create State name="California" \
-                   | ./console.py > salida')
-        self.assertTrue(os.path.isfile("file.json"))
-        # check if key name exist.
-        with open('file.json') as f:
-            data = json.load(f)
-            dict_value = data.values()
-            for value in dict_value:
-                self.assertTrue(('name' in value))
+    def test_console_module_docstring(self):
+        """ Test for the console.py module docstring """
+        self.assertIsNotNone(console.__doc__,
+                             "console.py file needs a docstrings")
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(__doc__) > 0, "console.py have docstrings")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstrings")
 
 
 if __name__ == '__main__':
