cb18435bb8b3b416a7f55e39b75ec8522a6d4046
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-21 10:54:29 -0500

    Fix script for update Nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,20 +3,14 @@
 
 if ! [ "$(command -v nginx)" ]
 then
-        sudo apt-get -y update && apt-get install -y nginx
-        sudo service nginx start
+        sudo apt-get update && apt-get install -y nginx
 fi
-
-if ! [ -d "/data" ]; then sudo mkdir -p /data/; fi
-if ! [ -d "/data/web_static/" ]; then sudo mkdir -p /data/web_static/; fi
-if ! [ -d "/data/web_static/releases/" ]; then sudo mkdir -p /data/web_static/releases/; fi
-if ! [ -d "/data/web_static/shared/" ]; then sudo mkdir -p /data/web_static/shared/; fi
+# Create the folder /data/, /data/web_static/ and /data/web_static/releases/ with a single command
 if ! [ -d "/data/web_static/releases/test/" ]; then sudo mkdir -p /data/web_static/releases/test/; fi
-sudo touch /data/web_static/releases/test/index.html
-echo "Fake HTML file :)" > /data/web_static/releases/test/index.html
-if [ -d "data/web_static/current" ]; then sudo rm -rf /data/web_static/current; fi
+if ! [ -d "/data/web_static/shared/" ]; then sudo mkdir -p /data/web_static/shared/; fi
+echo "Fake HTML File :)" | sudo tee /data/web_static/releases/test/index.html
 # Create a linked symbolic link, if it exists, it must be removed and recreated
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/ # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo sed -i '55 i \\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/>sudo service nginx restart
+sudo sed -i '55i \\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
