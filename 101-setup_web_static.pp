55496db14dfc18b6fc35bf282b236c0f20d78815
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-21 13:10:48 -0500

    Update Nginx with puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,18 +1,16 @@
 # Configuring Nginx with puppet
 
-exec { 'executing in nginx':
-  command  => 'if ! dpkg -s nginx > /dev/null;
+exec { 'Update Nginx':
+  command  => 'if ! [ "$(command -v nginx)" ];
                then apt-get -y update;
-               apt-get -y upgrade;
-               apt-get -y install nginx;
+               apt-get install nginx -y;
                fi;
                sudo mkdir -p /data/web_static/releases/test/;
                sudo mkdir -p /data/web_static/shared/;
-               echo Holberton School | sudo tee /data/web_static/releases/test/index.html;
+               echo "Fake HTML File :)" | sudo tee /data/web_static/releases/test/index.html;
                sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
                sudo chown -R ubuntu:ubuntu /data/;
-               sudo sed -i /:80 default_server;/ a \\\n\tlocation\
-            /hbnb_static {\n\talias /data/web_static/current/;\n\t}/\
+               sudo sed -i "55i \\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"\
             /etc/nginx/sites-available/default;
                sudo service nginx restart',
   provider => shell,
