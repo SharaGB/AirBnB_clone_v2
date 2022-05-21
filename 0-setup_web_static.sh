a2a3fdb6d8f7fc5a031f535fc0b3fd25b2e776b9
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-21 10:37:04 -0500

    Fix script for update Nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static.
 
-if [ -x "$(command -v nginx)" ];
+if ! [ "$(command -v nginx)" ]
 then
-        sudo apt update && apt install nginx -y
+        sudo apt-get -y update && apt-get install -y nginx
         sudo service nginx start
 fi
-# Create the folder /data/, /data/web_static/ and /data/web_static/releases/ with a single command
-if ! [ -d "/data/web_static/releases/test/" ]; then sudo mkdir -p /data/web_static/releases/test; fi
+
+if ! [ -d "/data" ]; then sudo mkdir -p /data/; fi
+if ! [ -d "/data/web_static/" ]; then sudo mkdir -p /data/web_static/; fi
+if ! [ -d "/data/web_static/releases/" ]; then sudo mkdir -p /data/web_static/releases/; fi
 if ! [ -d "/data/web_static/shared/" ]; then sudo mkdir -p /data/web_static/shared/; fi
-echo 'Fake HTML file :)' > /data/web_static/releases/test/index.html
+if ! [ -d "/data/web_static/releases/test/" ]; then sudo mkdir -p /data/web_static/releases/test/; fi
+sudo touch /data/web_static/releases/test/index.html
+echo " Fake HTML file :) " > /data/web_static/>
+if [ -d "data/web_static/current" ]; then sudo rm -rf /data/web_static/current; fi
 # Create a linked symbolic link, if it exists, it must be removed and recreated
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/ # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo sed -i '55i \\t\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-service nginx restart
+sudo sed -i '55 i \\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/>sudo service nginx restart
+sudo service nginx restart
