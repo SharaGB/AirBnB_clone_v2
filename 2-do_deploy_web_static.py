3854981e58abc3ddc0c409e6bb80e03c210c9d44
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-20 20:03:26 -0500

    Distributes an archive to your web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Distributes an archive to your web servers """
+from os.path import exists
+from fabric.api import env, put, run
+
+env.hosts = ['34.148.132.49', '3.95.23.58']
+
+
+def do_deploy(archive_path):
+    """ Distributes"""
+    if not exists(archive_path):
+        return False
+    try:
+        filename = archive_path.split('/')[-1].split(".")[0]
+        path = '/data/web_static/releases/{}'.format(filename)
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(filename, path))
+        run('rm /tmp/{}'.format(filename))
+        run('mv {}/web_static/* {}/'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(path))
+        print('New version deployed!')
+        return True
+    except Exception as e:
+        return False
