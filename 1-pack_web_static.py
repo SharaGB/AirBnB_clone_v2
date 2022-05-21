827893151060e7097f940c1d81990d1d1e1d54d9
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-20 20:02:48 -0500

    Generates a .tgz archive from the contents of the web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Fabric script that generates a .tgz archive from the
         contents of the web_static folder """
-from os import path
+from os.path import exists
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """ Function that generates tgz archive """
-    if not path.exists("versions"):
+    if not exists("versions"):
         local("mkdir versions")
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
