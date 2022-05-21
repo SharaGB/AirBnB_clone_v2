797ed4fe3e0b5108260d022041e6e8e7a9f62e07
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-21 11:12:15 -0500

    Fix distributes an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """ Distributes an archive to your web servers """
-from fabric.api import put, run, env
 from os.path import exists
+from fabric.api import env, put, run
 
 env.hosts = ['34.148.132.49', '3.95.23.58']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ Distributes"""
+    if not exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        file = archive_path.split('/')[-1].split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}/'.format(file))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'
+            .format(file, file))
+        run('rm /tmp/{}.tgz'.format(file))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}'.format(file, file))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(file))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        print('New version deployed!')
+        run('ln -s /data/web_static/releases/{}\
+            /data/web_static/current'.format(file))
+        print("New version deployed!")
         return True
-    except:
+
+    except Exception as e:
         return False
