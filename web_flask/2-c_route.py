42705ec98f0cff70c1f6a211069c62277e5277c7
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-23 14:26:18 -0500

    Add a route that receives two arguments

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Display “Hello HBNB!” """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display “HBNB” """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ Display “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space) """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
