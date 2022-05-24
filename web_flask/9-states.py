dada7a22c72b74ca59cda5b031c9d8fc5f06561c
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-24 15:47:49 -0500

    Display list states and id

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """close the conection with db"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """ Display a HTML page """
+    states = storage.all(State)
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """ Display a HTML page """
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
