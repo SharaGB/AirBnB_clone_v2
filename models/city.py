362696c17bb9b8f2d01a61e2698768b89b4acd67
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-24 13:41:47 -0500

    Add getter method

--- models/city.py
+++ models/city.py
@@ -4,17 +4,22 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
+from os import getenv
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
+        # Name of the Table
+        __tablename__ = 'cities'
 
-    # Name of the Table
-    __tablename__ = 'cities'
-
-    # Attributes/columns
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    # Relationships to other tables
-    places = relationship("Place", cascade="all, delete", backref="cities")
+        # Attributes/columns
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        # Relationships to other tables
+        places = relationship("Place", cascade="all, delete", backref="cities")
+else:
+    class City(BaseModel):
+        """ City class """
+        name = ''
+        state_id = ''
